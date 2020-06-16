cd72089bb931d1657ca5db016fe40eef0d1400b7
Dioniisan <misterioso2212@gmail.com>
2020-06-15 22:20:18 -0500

    migraciones habilitadas

---
+++ M-CHAT.Services/Migrations/20200616025344_Initial-Migration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace M_CHAT.Services.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cuentas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    CuentaID = table.Column<int>(nullable: false),
+                    Correo = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuentas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ninios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    NinioID = table.Column<int>(nullable: false),
+                    Nom_Comp = table.Column<string>(nullable: true),
+                    CURP = table.Column<string>(nullable: true),
+                    Fecha_nac = table.Column<string>(nullable: true),
+                    Edad = table.Column<int>(nullable: false),
+                    Genero = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ninios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CentrosE",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    CentroEID = table.Column<int>(nullable: false),
+                    Nombre = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Clave = table.Column<string>(nullable: true),
+                    NinioID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CentrosE", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CentrosE_Ninios_NinioID",
+                        column: x => x.NinioID,
+                        principalTable: "Ninios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tutores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedAT = table.Column<DateTime>(nullable: false),
+                    UpdatedAT = table.Column<DateTime>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    TutorID = table.Column<int>(nullable: false),
+                    Nombre = table.Column<string>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Correo = table.Column<string>(nullable: true),
+                    Contrase = table.Column<string>(nullable: true),
+                    NinioID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tutores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tutores_Ninios_NinioID",
+                        column: x => x.NinioID,
+                        principalTable: "Ninios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CentrosE_NinioID",
+                table: "CentrosE",
+                column: "NinioID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tutores_NinioID",
+                table: "Tutores",
+                column: "NinioID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CentrosE");
+
+            migrationBuilder.DropTable(
+                name: "Cuentas");
+
+            migrationBuilder.DropTable(
+                name: "Tutores");
+
+            migrationBuilder.DropTable(
+                name: "Ninios");
+        }
+    }
+}
--- M-CHAT.Services/SQLRepository.cs
+++ M-CHAT.Services/SQLRepository.cs
@@ -2,33 +2,54 @@
 using System.Collections.Generic;
 using System.Text;
 using M_CHAT.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace M_CHAT.Services
 {
     public class SQLRepository<T> : IRepository<T> where T : BaseEntity
     {
-        public SQLRepository()
+        protected readonly AppDBContext context;
+        private DbSet<T> entities;
+
+        public SQLRepository(AppDBContext context)
         {
+            this.context = context;
+            entities = context.Set<T>();
         }
         public void Delete(T obj)
         {
-            throw new NotImplementedException();
+            if (obj == null) throw new ArgumentNullException("Entity");
+            if (obj.Id <= 0) throw new ArgumentNullException("Entity");
+            obj.UpdatedAT = DateTime.Now;
+            obj.Status = false;
+            var entity = entities.Attach(obj);
+            entity.State = EntityState.Modified;
+            context.SaveChanges();
         }
         public T Get(int id)
         {
-            throw new NotImplementedException();
+            return entities.SingleOrDefault(x => x.Id == id);
         }
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            return entities.AsEnumerable();
         }
         public int Insert(T obj)
         {
-            throw new NotImplementedException();
+            if (obj == null) throw new ArgumentNullException("Entity");
+            entities.Add(obj);
+            context.SaveChanges();
+            return obj.Id;
         }
         public void Update(T obj)
         {
-            throw new NotImplementedException();
+            if (obj == null) throw new ArgumentNullException("Entity");
+            if (obj.Id <= 0) throw new ArgumentNullException("Entity");
+            var entity = entities.Attach(obj);
+            entity.State = EntityState.Modified;
+            context.SaveChanges();
         }
+
     }
 }
